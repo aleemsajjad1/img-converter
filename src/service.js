12fea6a8a5de1c638b08a3644050f05ec5fcd500
unknown <aleemsajjad1@gmail.com>
2023-04-18 14:57:23 +0500

    initial

---
+++ src/service.js
@@ -0,0 +1,16 @@
+let base_url = "http://localhost:8000/api/convert-to-"
+export function ConvrtFunction(endPoint, values) {
+
+    return new Promise((resolve, reject) => {
+      fetch(base_url + endPoint, {
+        method: 'POST',
+        body: values,
+      })
+        .then((response) => {
+            resolve(response.json());
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
